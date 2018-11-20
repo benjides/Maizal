08033ae29f226b6004be7dc9dba8976051c92c37
benjides <benjergara@gmail.com>
2018-11-20 21:49:00 +0100

    Engines bug on random search

--- src/engine/engines.js
+++ src/engine/engines.js
@@ -1,5 +1,5 @@
 const bfs = require('./uninformed/bfs');
 const dijkstra = require('./uninformed/dijkstra');
-const random = require('./uninformed/dijkstra');
+const random = require('./uninformed/random');
 
 module.exports = { bfs, dijkstra, random };
