61d1899b485e8db934276daac60df614ce0d629e
benjides <benjergara@gmail.com>
2018-12-02 16:13:51 +0100

    Refactor Dijkstra  unit tests

--- test/unit/uninformed/dijkstra.js
+++ test/unit/uninformed/dijkstra.js
@@ -10,17 +10,32 @@ chai.use(chaiAsPromised);
 let c = {};
 
 describe('Simple Dijkstra corridor search', () => {
-  it('A simple search should reach the last position in the corridor', () => expect(maizal.dijkstra(corridor).then(({ solution }) => solution.pop().data.position)).to.eventually.eq(4));
-  it('A better solution must prioritize over a worse one', () => {
+  it('A simple search should reach the last position in the corridor', async () => {
+    const { solution } = await maizal.dijkstra(corridor);
+    expect(solution.pop().data.position).to.eq(corridor.goals.position);
+  });
+  it('A better solution must prioritize over a worse one', async () => {
     c = Object.assign({}, corridor);
     c.goals = [{
       position: 2,
     }, {
       position: 4,
     }];
-    return expect(maizal.dijkstra(c).then(({ solution }) => solution.pop().data.position)).to.eventually.eq(2);
+    const { solution } = await maizal.dijkstra(c);
+    expect(solution.pop().data.position).to.eq(c.goals[0].position);
   });
-  it('Smaller cost nodes should be expanded first', () => {
+  it('A better solution must prioritize over a worse one, Dijkstra with action cost = 0 means a BFS so it should act as so', async () => {
+    // Dijkstra with action cost = 0 means a BFS so it should act as so
+    c = Object.assign({}, corridor);
+    c.goals = [{
+      position: 0,
+    }, {
+      position: 4,
+    }];
+    const { solution } = await maizal.dijkstra(c);
+    expect(solution.pop().data.position).to.eq(c.goals[0].position);
+  });
+  it('Smaller cost nodes should be expanded first', async () => {
     c = Object.assign({}, corridor);
     c.initial = {
       position: 3,
@@ -29,7 +44,7 @@ describe('Simple Dijkstra corridor search', () => {
       name: 'right',
       cost: 50,
       expand: (state) => {
-        if (state.position + 1 > 5) return undefined;
+        if (state.position + 1 > 4) return undefined;
         return { position: state.position + 1 };
       },
     },
@@ -40,95 +55,10 @@ describe('Simple Dijkstra corridor search', () => {
         return { position: state.position - 1 };
       },
     }];
-    return expect(maizal.dijkstra(c).then(({ stats }) => stats.nodes)).to.eventually.eq(4);
-  });
-  it('If the final states are not reachable the search should be rejected', () => {
-    c = Object.assign({}, corridor);
-    c.actions = {
-      expand: ({ position }) => Promise.resolve({ position }),
-    };
-    return expect(maizal.dijkstra(c).then(({ solution }) => solution)).to.be.rejected;
-  });
-  it('Chain two maizal searches', () => {
-    maizal.dijkstra(corridor);
-    c = Object.assign({}, corridor);
-    c.goals = {
-      position: 0,
-    };
-    return expect(maizal.dijkstra(c).then(({ solution }) => solution.pop().data.position)).to.eventually.eq(0);
-  });
-  describe('Initial set testing', () => {
-    it('If the intial is the goal should not expand any action', () => {
-      c = Object.assign({}, corridor);
-      c.goals = { position: 1 };
-      return expect(maizal.dijkstra(c).then(({ solution }) => solution)).to.eventually.have.length(1);
-    });
-    it('A search cannot start without an initial state', () => {
-      c = Object.assign({}, corridor);
-      delete c.initial;
-      return expect(maizal.dijkstra(c).then(({ solution }) => solution)).to.be.rejected;
-    });
-    it('A search cannot start with an empty initial state', () => {
-      c = Object.assign({}, corridor);
-      c.initial = {};
-      return expect(maizal.dijkstra(c).then(({ solution }) => solution)).to.be.rejected;
-    });
-  });
-  describe('Action testing', () => {
-    it('Promise actions are supported and the robot shall reach the end of the corridor', () => {
-      c = Object.assign({}, corridor);
-      c.actions = {
-        expand: ({ position }) => Promise.resolve({ position: position + 1 }),
-      };
-      return expect(maizal.dijkstra(c).then(({ solution }) => solution.pop().data.position)).to.eventually.eq(4);
-    });
-    it('A search with no actions should be rejected', () => {
-      c = Object.assign({}, corridor);
-      c.actions = {};
-      return expect(maizal.dijkstra(c).then(({ solution }) => solution)).to.be.rejected;
-    });
-    it('An empty expand function should be rejected', () => {
-      c = Object.assign({}, corridor);
-      c.actions = {
-        name: 'IdoNotHaveExpand',
-      };
-      return expect(maizal.dijkstra(c).then(({ solution }) => solution)).to.be.rejected;
-    });
-    it('Actions returning several states are allowed', () => {
-      c = Object.assign({}, corridor);
-      c.actions = {
-        expand: ({ position }) => {
-          if (position > 5 || position < 0) {
-            return undefined;
-          }
-          return [{ position: position + 1 }, { position: position - 1 }];
-        },
-      };
-      return expect(maizal.dijkstra(c).then(({ solution }) => solution.pop().data.position)).to.eventually.eq(4);
-    });
-  });
-  describe('Hash testing', () => {
-    it('Function hashes are allowed', () => {
-      c = Object.assign({}, corridor);
-      c.hash = ({ position }) => position + 15;
-      return expect(maizal.dijkstra(c).then(({ solution }) => solution)).to.eventually.have.length(4);
-    });
-    it('A search with no hash should be rejected', () => {
-      c = Object.assign({}, corridor);
-      delete c.hash;
-      return expect(maizal.dijkstra(c).then(({ solution }) => solution)).to.be.rejected;
-    });
-    it('A non existing hash should be rejected', () => {
-      c = Object.assign({}, corridor);
-      c.hash = 'IdoNotExist';
-      return expect(maizal.dijkstra(c).then(({ solution }) => solution)).to.be.rejected;
-    });
-  });
-  describe('Goals testing', () => {
-    it('A search with no goals should be rejected', () => {
-      c = Object.assign({}, corridor);
-      delete c.goals;
-      return expect(maizal.dijkstra(c).then(({ solution }) => solution)).to.be.rejected;
-    });
+    const { solution, stats } = await maizal.dijkstra(c);
+    expect(solution.pop().data.position).to.eq(c.goals.position);
+    expect(stats.nodes).to.eq(4);
+    expect(stats.depth).to.eq(1);
+    expect(stats.cost).to.eq(50);
   });
 });
