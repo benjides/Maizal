c91414290051206bc040a3bffbb6542c4a45c198
benjides <benjergara@gmail.com>
2025-11-16 19:21:30 +0100

    Add insert and tests

--- src/PriorityQueue.ts
+++ src/PriorityQueue.ts
@@ -17,3 +17,24 @@ export const priorityQueue =
 
 export const values = <T>(priorityQueue: PriorityQueue<T>) =>
   priorityQueue.map((node: Node<T>) => node.data)
+
+export const insert =
+  <T>(priority: (node: T) => number) =>
+  (value: T) =>
+  (priorityQueue: PriorityQueue<T>) => {
+    const nodeToInsert: Node<T> = {
+      priority: priority(value),
+      data: value,
+    }
+
+    const index: number = priorityQueue.findIndex(
+      (node: Node<T>) => nodeToInsert.priority < node.priority,
+    )
+
+    if (index === -1) return [...priorityQueue, nodeToInsert]
+    return [
+      ...priorityQueue.slice(0, index),
+      nodeToInsert,
+      ...priorityQueue.slice(index),
+    ]
+  }
--- tests/PriorityQueue.test.ts
+++ tests/PriorityQueue.test.ts
@@ -1,5 +1,5 @@
 import { expect, it } from 'vitest'
-import { priorityQueue, values } from '../src/PriorityQueue.js'
+import { insert, priorityQueue, values } from '../src/PriorityQueue.js'
 
 type Month = {
   index: number
@@ -36,7 +36,37 @@ it('sorts values on creation', () => {
     {
       index: 1,
       name: 'February',
-    }
+    },
   ]
   expect(priorityQueueValues).toStrictEqual(expectedPriorityQueueValues)
+})
+
+it('inserts value at last position', () => {
+  const months: Month[] = [
+    {
+      index: 1,
+      name: 'February',
+    },
+  ]
+
+  const month: Month = {
+    index: 2,
+    name: 'March',
+  }
+
+  const priority = (month: Month) => month.index
+
+  const priorityQueueValues = insert(priority)(month)(priorityQueue(priority)(months))
+
+  const expectedPriorityQueueValues: Month[] = [
+    {
+      index: 1,
+      name: 'February',
+    },
+    {
+      index: 2,
+      name: 'March',
+    },
+  ]
+  expect(values(priorityQueueValues)).toStrictEqual(expectedPriorityQueueValues)
 })
