a78ffc73f5eb9ff51db414a5d55004a779e3ed2c
benjides <benjergara@gmail.com>
2025-11-18 20:41:53 +0100

    Organise tests

--- tests/HashSet.test.ts
+++ tests/HashSet.test.ts
@@ -7,84 +7,94 @@ type Vector = {
   y: number
 }
 
-describe('HashSet creation', () => {
-  it('creates empty HashSet', () => {
-    assert.deepStrictEqual(empty<Vector>(), [])
+describe('HashSet', () => {
+  describe('constructor', () => {
+    it('creates empty', () => {
+      assert.deepStrictEqual(empty<Vector>(), [])
+    })
   })
-})
 
-describe('HashSet insert', () => {
-  it('inserts value', () => {
-    const vector: Vector = {
-      x: -3,
-      y: 8,
-    }
-
-    const hashSet = insert(vector)(empty())
-
-    const expectedHashSetValues: Vector[] = [
-      {
+  describe('insert', () => {
+    it('inserts value', () => {
+      const vector: Vector = {
         x: -3,
         y: 8,
-      },
-    ]
-    assert.deepStrictEqual(hashSet, expectedHashSetValues)
-  })
-})
+      }
 
-describe('PriorityQueue has', () => {
-  it('does not have element for an empty HashSet', () => {
-    const hashSet = empty<Vector>()
+      const hashSet = insert(vector)(empty())
 
-    const hasVector = has((a: Vector, b: Vector) => a.x === b.x && b.y === b.y)
-
-    const vector: Vector = {
-      x: -3,
-      y: 8,
-    }
-    assert.isFalse(hasVector(vector)(hashSet))
+      const expectedHashSetValues: Vector[] = [
+        {
+          x: -3,
+          y: 8,
+        },
+      ]
+      assert.deepStrictEqual(hashSet, expectedHashSetValues)
+    })
   })
 
-  it('has element for an element present in the HashSet', () => {
-    const vector: Vector = {
-      x: -3,
-      y: 8,
-    }
+  describe('has', () => {
+    it('does not have element for an empty HashSet', () => {
+      const hashSet = empty<Vector>()
 
-    const hashSet = insert(vector)(empty())
+      const hasVector = has(
+        (a: Vector, b: Vector) => a.x === b.x && b.y === b.y,
+      )
 
-    const hasVector = has((a: Vector, b: Vector) => a.x === b.x && b.y === b.y)
+      const vector: Vector = {
+        x: -3,
+        y: 8,
+      }
+      assert.isFalse(hasVector(vector)(hashSet))
+    })
 
-    assert.isTrue(hasVector(vector)(hashSet))
-  })
+    it('has element for an element present in the HashSet', () => {
+      const vector: Vector = {
+        x: -3,
+        y: 8,
+      }
 
-  it('has element for an element present in the HashSet with swapped properties', () => {
-    const vector: Vector = {
-      x: -3,
-      y: 8,
-    }
+      const hashSet = insert(vector)(empty())
 
-    const hashSet = insert(vector)(empty())
+      const hasVector = has(
+        (a: Vector, b: Vector) => a.x === b.x && b.y === b.y,
+      )
 
-    const hasVector = has((a: Vector, b: Vector) => a.x === b.x && b.y === b.y)
+      assert.isTrue(hasVector(vector)(hashSet))
+    })
 
-    const swappedPropertiesVector: Vector = {
-      y: 8,
-      x: -3,
-    }
-    assert.isTrue(hasVector(swappedPropertiesVector)(hashSet))
-  })
+    it('has element for an element present in the HashSet with swapped properties', () => {
+      const vector: Vector = {
+        x: -3,
+        y: 8,
+      }
 
-  it('not present HashSet', () => {
-    const vector: Vector = {
-      x: -3,
-      y: 8,
-    }
+      const hashSet = insert(vector)(empty())
 
-    const hashSet = insert(vector)(empty())
+      const hasVector = has(
+        (a: Vector, b: Vector) => a.x === b.x && b.y === b.y,
+      )
 
-    const hasVector = has((a: Vector, b: Vector) => a.x === b.x && b.y === b.y)
+      const swappedPropertiesVector: Vector = {
+        y: 8,
+        x: -3,
+      }
+      assert.isTrue(hasVector(swappedPropertiesVector)(hashSet))
+    })
 
-    assert.isFalse(hasVector({ x: 1, y: -18 })(hashSet))
+    it('not present HashSet', () => {
+      const vector: Vector = {
+        x: -3,
+        y: 8,
+      }
+
+      const hashSet = insert(vector)(empty())
+
+      const hasVector = has(
+        (a: Vector, b: Vector) => a.x === b.x && b.y === b.y,
+      )
+
+      assert.isFalse(hasVector({ x: 1, y: -18 })(hashSet))
+    })
   })
 })
