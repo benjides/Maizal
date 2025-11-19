f6ac73736a9df93ef83f23e9e8a2c9f8bbe3d439
benjides <benjergara@gmail.com>
2025-11-19 11:16:27 +0100

    test it solves after expanding once

--- src/DepthFirstSearch.ts
+++ src/DepthFirstSearch.ts
@@ -1,6 +1,65 @@
+import {
+  insert as insertQueue,
+  poll,
+  priorityQueue,
+  type PriorityQueue,
+} from './PriorityQueue.js'
+
 export type Eq<S> = (a: S, b: S) => boolean
 
-export type Search = <S>(initial: S, goal: S, eq: Eq<S>) => Promise<S[]>
+export type Expand<S> = (s: S) => Promise<S>
 
-export const depthFirstSearch: Search = <S>(initial: S, goal: S, eq: Eq<S>) =>
-  eq(initial, goal) ? Promise.resolve([goal]) : Promise.resolve([])
+export type Search = <S>(
+  initial: S,
+  goal: S,
+  eq: Eq<S>,
+  expand: Expand<S>,
+) => Promise<S[]>
+
+type State<S> = {
+  parent: State<S> | null
+  state: S
+  depth: number
+}
+
+export const depthFirstSearch: Search = <S>(
+  initial: S,
+  goal: S,
+  eq: Eq<S>,
+  expand: Expand<S>,
+): Promise<S[]> => {
+  const open: PriorityQueue<State<S>> = priorityQueue([])
+
+  async function expandRecursively(
+    open: PriorityQueue<State<S>>,
+    s: State<S>,
+  ): Promise<S[]> {
+    if (eq(goal, s.state)) {
+      if (s.parent !== null) {
+        return [s.state, s.parent.state].reverse()
+      }
+
+      return [s.state]
+    }
+
+    const ns: S = await expand(s.state)
+
+    const data: State<S> = {
+      parent: s,
+      depth: s.depth + 1,
+      state: ns,
+    }
+    const queue = insertQueue({
+      priority: s.depth + 1,
+      data: data,
+    })(open)
+    return expandRecursively(queue, poll(queue)[0] as State<S>)
+  }
+  const initState: State<S> = {
+    depth: 0,
+    parent: null,
+    state: initial,
+  }
+
+  return expandRecursively(open, initState)
+}
--- tests/DepthFirstSearch.test.ts
+++ tests/DepthFirstSearch.test.ts
@@ -18,10 +18,38 @@ describe('DepthFirstSearch', () => {
     const eq: Eq<Position> = (a: Position, b: Position) =>
       a.y === b.y && a.x === b.x
 
-    const actualSolution = await depthFirstSearch(initial, goal, eq)
+    const actualSolution = await depthFirstSearch(initial, goal, eq, () =>
+      Promise.resolve(goal),
+    )
 
     const expectedSolution: Position[] = [{ x: 0, y: 0 }]
+    assert.deepStrictEqual(actualSolution, expectedSolution)
+  })
 
+  it('solves after expanding once', async () => {
+    type Position = {
+      x: number
+      y: number
+    }
+    const initial: Position = {
+      x: 0,
+      y: 0,
+    }
+    const goal: Position = {
+      x: 0,
+      y: 1,
+    }
+    const eq: Eq<Position> = (a: Position, b: Position) =>
+      a.y === b.y && a.x === b.x
+
+    const actualSolution = await depthFirstSearch(initial, goal, eq, () =>
+      Promise.resolve(goal),
+    )
+
+    const expectedSolution: Position[] = [
+      { x: 0, y: 0 },
+      { x: 0, y: 1 },
+    ]
     assert.deepStrictEqual(actualSolution, expectedSolution)
   })
 })
