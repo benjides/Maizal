1a5fd4d58ccaf339177fffde8d63c8f55c58c1f1
benjides <benjergara@gmail.com>
2025-11-19 21:33:45 +0100

    refactor: reorganise tests

--- tests/DepthFirstSearch.test.ts
+++ tests/DepthFirstSearch.test.ts
@@ -13,6 +13,42 @@ type Position = {
 const eq: Eq<Position> = (a: Position, b: Position) =>
   a.y === b.y && a.x === b.x
 
+type Movement = (position: Position) => Position | null
+
+const up: Movement = (position: Position) => {
+  if (position.y === 2) {
+    return null
+  }
+  return {
+    x: position.x,
+    y: position.y + 1,
+  }
+}
+const down: Movement = (position: Position) => {
+  if (position.y === 0) {
+    return null
+  }
+  return {
+    x: position.x,
+    y: position.y - 1,
+  }
+}
+const right: Movement = (position: Position) => {
+  if (position.x === 2) {
+    return null
+  }
+
+  return {
+    x: position.x + 1,
+    y: position.y,
+  }
+}
+const expand: Expand<Position> = (position: Position) =>
+  [up, down, right]
+    .map((movement: Movement) => movement(position))
+    .filter((position: Position | null) => position !== null)
+    .map((position: Position) => Promise.resolve(position))
+
 describe('DepthFirstSearch', () => {
   it('solves when initial and goal are equals', async () => {
     const initial: Position = {
@@ -24,9 +60,7 @@ describe('DepthFirstSearch', () => {
       y: 0,
     }
 
-    const actualSolution = await depthFirstSearch(initial, goal, eq, () => [
-      Promise.resolve(goal),
-    ])
+    const actualSolution = await depthFirstSearch(initial, goal, eq, expand)
 
     const expectedSolution: Position[] = [{ x: 0, y: 0 }]
     assert.deepStrictEqual(actualSolution, expectedSolution)
@@ -42,9 +76,7 @@ describe('DepthFirstSearch', () => {
       y: 1,
     }
 
-    const actualSolution = await depthFirstSearch(initial, goal, eq, () => [
-      Promise.resolve(goal),
-    ])
+    const actualSolution = await depthFirstSearch(initial, goal, eq, expand)
 
     const expectedSolution: Position[] = [
       { x: 0, y: 0 },
@@ -63,54 +95,12 @@ describe('DepthFirstSearch', () => {
       y: 2,
     }
 
-    const actualSolution = await depthFirstSearch(
-      initial,
-      goal,
-      eq,
-      (a: Position) => [
-        Promise.resolve({
-          x: a.x,
-          y: a.y + 1,
-        }),
-      ],
-    )
-
-    const expectedSolution: Position[] = [
-      { x: 0, y: 0 },
-      { x: 0, y: 1 },
-      { x: 0, y: 2 },
-    ]
-    assert.deepStrictEqual(actualSolution, expectedSolution)
-  })
-
-  it('solves after expanding multiple states', async () => {
-    const initial: Position = {
-      x: 0,
-      y: 0,
-    }
-    const goal: Position = {
-      x: 0,
-      y: 1,
-    }
-    type Movement = (position: Position) => Position
-    const up: Movement = (position: Position) => ({
-      x: position.x,
-      y: position.y + 1,
-    })
-    const right: Movement = (position: Position) => ({
-      x: position.x + 1,
-      y: position.y,
-    })
-    const expand: Expand<Position> = (position: Position) =>
-      [up, right]
-        .map((movement: Movement) => movement(position))
-        .map((position: Position) => Promise.resolve(position))
-
     const actualSolution = await depthFirstSearch(initial, goal, eq, expand)
 
     const expectedSolution: Position[] = [
       { x: 0, y: 0 },
       { x: 0, y: 1 },
+      { x: 0, y: 2 },
     ]
     assert.deepStrictEqual(actualSolution, expectedSolution)
   })
@@ -122,81 +112,15 @@ describe('DepthFirstSearch', () => {
     }
     const goal: Position = {
       x: 1,
-      y: 1,
+      y: 2,
     }
-    type Movement = (position: Position) => Position | null
-    const up: Movement = (position: Position) => {
-      if (position.y === 1) {
-        return null
-      }
-      return {
-        x: position.x,
-        y: position.y + 1,
-      }
-    }
-    const right: Movement = (position: Position): Position => ({
-      x: position.x + 1,
-      y: position.y,
-    })
-    const expand: Expand<Position> = (position: Position) =>
-      [up, right]
-        .map((movement: Movement) => movement(position))
-        .filter((position: Position | null) => position !== null)
-        .map((position: Position) => Promise.resolve(position))
 
     const actualSolution = await depthFirstSearch(initial, goal, eq, expand)
 
     const expectedSolution: Position[] = [
       { x: 0, y: 1 },
-      { x: 1, y: 1 },
-    ]
-    assert.deepStrictEqual(actualSolution, expectedSolution)
-  })
-
-  it('solves filtering already visited states', async () => {
-    const initial: Position = {
-      x: 0,
-      y: 0,
-    }
-    const goal: Position = {
-      x: 1,
-      y: 1,
-    }
-    type Movement = (position: Position) => Position | null
-    const up: Movement = (position: Position) => {
-      if (position.y === 1) {
-        return null
-      }
-      return {
-        x: position.x,
-        y: position.y + 1,
-      }
-    }
-    const down: Movement = (position: Position) => {
-      if (position.y === 0) {
-        return null
-      }
-      return {
-        x: position.x,
-        y: position.y - 1,
-      }
-    }
-    const right: Movement = (position: Position): Position => ({
-      x: position.x + 1,
-      y: position.y,
-    })
-    const expand: Expand<Position> = (position: Position) =>
-      [up, down, right]
-        .map((movement: Movement) => movement(position))
-        .filter((position: Position | null) => position !== null)
-        .map((position: Position) => Promise.resolve(position))
-
-    const actualSolution = await depthFirstSearch(initial, goal, eq, expand)
-
-    const expectedSolution: Position[] = [
-      { x: 0, y: 0 },
-      { x: 0, y: 1 },
-      { x: 1, y: 1 },
+      { x: 0, y: 2 },
+      { x: 1, y: 2 },
     ]
     assert.deepStrictEqual(actualSolution, expectedSolution)
   })
