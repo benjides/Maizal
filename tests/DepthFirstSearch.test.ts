51d227b72b92e68f9fd6fb72a4a278501ca32939
benjides <benjergara@gmail.com>
2025-11-19 14:39:37 +0100

    test it solves after expanding multiple states filtering invalid movements

--- tests/DepthFirstSearch.test.ts
+++ tests/DepthFirstSearch.test.ts
@@ -114,4 +114,42 @@ describe('DepthFirstSearch', () => {
     ]
     assert.deepStrictEqual(actualSolution, expectedSolution)
   })
+
+  it('solves after expanding multiple states filtering invalid movements', async () => {
+    const initial: Position = {
+      x: 0,
+      y: 1,
+    }
+    const goal: Position = {
+      x: 1,
+      y: 1,
+    }
+    type Movement = (position: Position) => Position | null
+    const up: Movement = (position: Position) => {
+      if (position.y === 1) {
+        return null
+      }
+      return {
+        x: position.x,
+        y: position.y + 1,
+      }
+    }
+    const right: Movement = (position: Position): Position => ({
+      x: position.x + 1,
+      y: position.y,
+    })
+    const expand: Expand<Position> = (position: Position) =>
+      [up, right]
+        .map((movement: Movement) => movement(position))
+        .filter((position: Position | null) => position !== null)
+        .map((position: Position) => Promise.resolve(position))
+
+    const actualSolution = await depthFirstSearch(initial, goal, eq, expand)
+
+    const expectedSolution: Position[] = [
+      { x: 0, y: 1 },
+      { x: 1, y: 1 },
+    ]
+    assert.deepStrictEqual(actualSolution, expectedSolution)
+  })
 })
