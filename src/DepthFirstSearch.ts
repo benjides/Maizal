27fd9e16d843219f12fec1e175693fa1f6234ad5
benjides <benjergara@gmail.com>
2025-11-21 18:51:28 +0100

    Use Namespaced imports

--- src/DepthFirstSearch.ts
+++ src/DepthFirstSearch.ts
@@ -1,17 +1,9 @@
-import {
-  insert as insertQueue,
-  poll,
-  priorityQueue,
-  type PriorityQueue,
-} from './PriorityQueue.js'
-import { empty, has, type HashSet, insert } from './HashSet.js'
-import {
-  type Child,
-  toArray,
-  type Tree,
-  insert as insertChild,
-  tree,
-} from './Tree.js'
+import * as PQ from './PriorityQueue.js'
+import * as T from './Tree.js'
+import * as HS from './HashSet.js'
+import type { PriorityQueue } from './PriorityQueue.js'
+import type { Child, Tree } from './Tree.js'
+import type { HashSet } from './HashSet.js'
 
 export type Eq<S> = (a: S, b: S) => boolean
 
@@ -30,14 +22,14 @@ export const depthFirstSearch: Search = <S>(
   eq: Eq<S>,
   expand: Expand<S>,
 ): Promise<S[]> => {
-  const open: PriorityQueue<Tree<number, S>> = priorityQueue([])
-  const closed: HashSet<S> = empty()
+  const open: PriorityQueue<Tree<number, S>> = PQ.priorityQueue([])
+  const closed: HashSet<S> = HS.empty()
 
   async function expandRecursively(
     open: PriorityQueue<Tree<number, S>>,
     closed: HashSet<S>,
   ): Promise<S[]> {
-    const r = poll(open)
+    const r = PQ.poll(open)
 
     const next = r[0]
     let nextQueue = r[1]
@@ -47,11 +39,11 @@ export const depthFirstSearch: Search = <S>(
     }
 
     if (eq(goal, next.value)) {
-      return toArray(next)
+      return T.toArray(next)
     }
 
     const newStates = (await Promise.all(expand(next.value)))
-      .filter((state: S) => !has(eq)(state)(closed))
+      .filter((state: S) => !HS.has(eq)(state)(closed))
       .map(
         (state: S): Child<number, S> => ({
           key: next.key + 1,
@@ -60,24 +52,24 @@ export const depthFirstSearch: Search = <S>(
       )
 
     for (const newState of newStates) {
-      const tree: Tree<number, S> = insertChild(newState)(next)
-      nextQueue = insertQueue({
+      const tree: Tree<number, S> = T.insert(newState)(next)
+      nextQueue = PQ.insert({
         priority: -newState.key,
         data: tree,
       })(nextQueue)
     }
 
-    closed = insert(next.value)(closed)
+    closed = HS.insert(next.value)(closed)
     return expandRecursively(nextQueue, closed)
   }
 
-  const t: Tree<number, S> = tree({
+  const t: T.Tree<number, S> = T.tree({
     key: 0,
     value: initial,
   })
 
   return expandRecursively(
-    insertQueue({
+    PQ.insert({
       data: t,
       priority: 0,
     })(open),
