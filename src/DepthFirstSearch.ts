435b5fb9fb4b75f090013c7fbb676126da01b7d9
benjides <benjergara@gmail.com>
2025-11-29 01:09:52 +0100

    Add State<S> type to represent a Search exploration status and usage

--- src/DepthFirstSearch.ts
+++ src/DepthFirstSearch.ts
@@ -13,6 +13,30 @@ export type Search = <S>(
   expand: Expand<S>,
 ) => Promise<S[]>
 
+type Node<S> = T.Tree<number, S>
+
+type State<S> = {
+  current: Node<S>
+  open: PQ.PriorityQueue<Node<S>>
+  closed: HS.HashSet<S>
+}
+
+export const poll: <T>(state: State<T>) => State<T> | null = <T>(
+  state: State<T>,
+) => {
+  const [currentState, priorityQueue] = PQ.poll(state.open)
+
+  if (currentState === null) {
+    return null
+  }
+
+  return {
+    current: currentState,
+    closed: HS.insert(currentState.value)(state.closed),
+    open: priorityQueue,
+  }
+}
+
 export const depthFirstSearch: Search = async <S>(
   initial: S,
   goal: S,
@@ -34,32 +58,36 @@ const expandRecursively = async <S>(
   eq: Eq<S>,
   expand: Expand<S>,
 ): Promise<S[]> => {
-  const [currentState, priorityQueue] = PQ.poll(open)
+  const state: State<S> = {
+    current: T.fromRoot(1, goal),
+    open: open,
+    closed: closed,
+  }
+  const ns = poll(state)
 
-  if (currentState === null) {
+  if (ns === null) {
     return []
   }
-  closed = HS.insert(currentState.value)(closed)
 
-  if (eq(goal, currentState.value)) {
-    return T.toArray(currentState)
+  if (eq(goal, ns.current.value)) {
+    return T.toArray(ns.current)
   }
 
   const newStates: PQ.PriorityQueue<T.Tree<number, S>> = (
-    await Promise.all(expand(currentState.value))
+    await Promise.all(expand(ns.current.value))
   )
-    .filter((state: S) => !HS.has(eq)(state)(closed))
+    .filter((state: S) => !HS.has(eq)(state)(ns.closed))
     .map(
       (state: S): T.Tree<number, S> =>
-        T.insert(currentState.key - 1, state)(currentState),
+        T.insert(ns.current.key - 1, state)(ns.current),
     )
     .reduce(
       (
         priorityQueue: PQ.PriorityQueue<T.Tree<number, S>>,
         treeBranch: T.Tree<number, S>,
       ) => PQ.insert(treeBranch.key, treeBranch)(priorityQueue),
-      priorityQueue,
+      ns.open,
     )
 
-  return expandRecursively(goal, newStates, closed, eq, expand)
+  return expandRecursively(goal, newStates, ns.closed, eq, expand)
 }
